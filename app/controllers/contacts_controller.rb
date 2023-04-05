efb9e659544922a7d352a4e6c585f9b16fc93062
SHANE RILEY <shaneriley0013@gmail.com>
2023-04-05 17:59:13 -0500

    add patch hardcode w/ Geocoder

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -17,12 +17,18 @@ class ContactsController < ApplicationController
   end  
 
   def create
+    results = Geocoder.search(params[:address])
+    latitude = results.first.coordinates[0]
+    longitude = results.first.coordinates[1]
+
     @contact = Contact.new(
       first_name: params[:first_name], 
       last_name: params[:last_name], 
       email: params[:email], 
       phone_number: params[:phone_number],
-      address: params[:address]      
+      address: params[:address], 
+      lat: latitude, 
+      lng: longitude,     
     )
     if @contact.save
       render :show
@@ -33,12 +39,18 @@ class ContactsController < ApplicationController
   end
   
   def update
+    results = Geocoder.search(params[:address])
+    latitude = results.first.coordinates[0]
+    longitude = results.first.coordinates[1]
+
     @contact = Contact.find_by(id: params[:id])
     @contact.first_name = params[:first_name] || @contact.first_name
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
     @contact.address = params[:address] || @contact.address
+    @contact.lat = params[:lat] || @contact.lat
+    @contact.lng = params[:lng] || @contact.lng    
     
     if @contact.save
       render :show
